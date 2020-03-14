30a5e784e0cd7f1fb2ec731c92f33116a8692ce9
Aaron Castro <aaroncrcr@gmail.com>
2020-03-14 16:12:20 -0300

    Hacer que la clase Base sea abstracta #1

--- src/app/shared/models/base.ts
+++ src/app/shared/models/base.ts
@@ -1,3 +1,13 @@
 export class Base {
-  id: number;
+
+  private _id: number;
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public set id(id: number) {
+    this._id = id;
+  }
+
 }
